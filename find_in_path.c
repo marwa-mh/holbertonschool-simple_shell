f2883a9ab87e350ecee74c4c85e20f3560bf19fb
Ian <Gibbu_@hotmail.com>
2025-04-22 11:41:35 +1000

    Fix? No.3

--- find_in_path.c
+++ find_in_path.c
@@ -33,25 +33,33 @@ char *_getenv(const char *name)
 char *find_in_path(const char *command)
 {
     char *path = _getenv("PATH");
-    char *path_copy = strdup(path);
+    char *path_copy;
     char *dir = strtok(path_copy, ":");
     char full_path[1024];
 
-    while (dir != NULL)
-    {
-        full_path[0] = '\0';
-        
-        strcpy(full_path, dir);
-        strcat(full_path, "/");
-        strcat(full_path, command);
+	if (path == NULL || *path == '\0')
+		return (NULL);
 
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return strdup(full_path);
-        }
-        dir = strtok(NULL, ":");
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
+
+  while (dir != NULL)
+  {
+		full_path[0] = '\0';
+
+    strcpy(full_path, dir);
+    strcat(full_path, "/");
+    strcat(full_path, command);
+
+    if (access(full_path, X_OK) == 0)
+    {
+      free(path_copy);
+				return strdup(full_path);
     }
-    free(path_copy);
-    return NULL;
+    dir = strtok(NULL, ":");
+  }
+
+  free(path_copy);
+  return (NULL);
 }
